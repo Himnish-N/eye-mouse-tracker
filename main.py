102110439abccc7885d1b7a3f5f3c2b559785602
Himnish-N <pes1202101226@pesu.pes.edu>
2024-03-13 20:53:53 +0530

    movement of mouse pointer with movement of eyes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 import mediapipe as mp
+import pyautogui
 
 
 cam = cv2.VideoCapture(1) # start the webcam
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks = True) # load the face mesh model
+screen_width, screen_height = pyautogui.size() # get the screen size
 
 while True:
     _, frame = cam.read() # read the frame from the webcam
@@ -15,10 +17,15 @@ while True:
     frame_height, frame_width, _ = frame.shape
     if landmarks_points:
         landmarks = landmarks_points[0].landmark
-        for landmark in landmarks[474:478]:
+        for id, landmark in enumerate(landmarks[474:478]):
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
             cv2.circle(frame, (x, y), 1, (0, 255, 0))
-            print(x, y)
+            #print(x, y)
+            if id == 0:
+                screen_x = screen_width/ frame_width * x
+                screen_y = screen_height/ frame_height * y
+                #x1, y1 = x, y
+                pyautogui.moveTo(screen_x, screen_y) 
     cv2.imshow('Eye control mouse', frame)
     cv2.waitKey(1)
